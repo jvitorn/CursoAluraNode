edfeab7b78af00269884f9fdd4245ad34bf06efc
jvitorn <joaoeuno2011@gmail.com>
2020-04-09 16:45:43 -0300

    5 - implementando a remocao de livros e mais funcoes em livro-dao

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -36,8 +36,29 @@ module.exports = (app) => {
                 .then(resp.redirect('/livros'))
                 .catch(erro => console.log(erro));
     }); 
-    app.get('/livros/form',(req,resp)=>{
-        resp.marko(require('../views/livros/form/form.marko'));
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
+    });
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+    
+        livroDao.buscaPorId(id)
+            .then(livro => 
+                resp.marko(
+                    require('../views/livros/form/form.marko'),
+                    { livro: livro }
+                )
+            )
+            .catch(erro => console.log(erro));
+    
     });
 
+    app.delete('/livros/:id',(req,resp)=>{
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id)
+                .then(()=>resp.status(200).end())
+                .catch(error => console.log(error));
+    });
 }
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -4,9 +4,13 @@ require('marko/express');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({extended:true}));
 const rotas = require('../app/rotas/rotas.js');
 
+app.use('/estatico',express.static('src/app/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
 rotas(app);
+
 module.exports = app;
